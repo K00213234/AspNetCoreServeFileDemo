2687217a0dbf2fc50f5f07d358e205d3c1b436b2
Mark <k00213234@gmail.com>
2020-05-01 21:01:58 +0100

    Initial GIT upload

---
+++ Controllers/AppController.cs
@@ -0,0 +1,14 @@
+﻿namespace ServeFileDemo.Controllers
+{
+	using Microsoft.AspNetCore.Mvc;
+	/// <summary>
+	/// Controller for App
+	/// </summary>
+	public class AppController : Controller
+	{
+		public IActionResult Index()
+		{
+			return this.View();
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+namespace ServeFileDemo
+{
+	using System;
+	using Microsoft.AspNetCore.Hosting;
+	using Microsoft.Extensions.Hosting;
+
+	public class Program
+	{
+		public static void Main(String[] args)
+		{
+			CreateHostBuilder(args).Build().Run();
+		}
+
+		public static IHostBuilder CreateHostBuilder(String[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+				});
+	}
+}
